18b2aef634f7d976a2eb5b871066042ff1e74b7a
alexisrhc <alexisrhc@hotmail.com>
2019-09-21 12:05:06 -0400

    update

---
+++ resources/views/client/index.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.appAdmin')
+
+@section('content')
+    <div class="row">
+        <div class="col-md-12 text-right">
+        	<button class="btn btn-sm btn-success">
+        		Agregar Cliente
+        		<i class="ni ni-fat-add text-yellow"></i>
+        	</button>
+        </div>
+    </div>
+@endsection
