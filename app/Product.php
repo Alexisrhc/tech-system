64f0e55b789a6d4b60cb9d382c5ecb2ddc348e8a
Luis Palma <palmaluis1997@gmail.com>
2019-09-22 10:06:32 -0430

    crud product

--- app/Product.php
+++ app/Product.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    protected $primaryKey = 'id_product';
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['id_product', 'code', 'serial', 'name', 'description', 'quantity', 'price'];
 }
