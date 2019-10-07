ecd533e2dd24299a3156727468a8e62a10a2a07f
alexisrhc <alexisrhc@hotmail.com>
2019-10-07 00:34:11 -0300

    delete status on bill

--- database/migrations/2019_09_19_013907_create_bills_table.php
+++ database/migrations/2019_09_19_013907_create_bills_table.php
@@ -21,7 +21,6 @@ class CreateBillsTable extends Migration
             $table->foreign('id_client')->references('id_client')->on('clients');
             $table->unsignedBigInteger('id_bill_temporal');
             $table->foreign('id_bill_temporal')->references('id_bill_temporal')->on('bill_temporals');
-            $table->enum('status', ['pendind','paid','cancelled']);
             $table->timestamps();
         });
     }
