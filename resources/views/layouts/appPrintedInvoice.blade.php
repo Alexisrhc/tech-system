1cb0f256342bb2420423c874753e3914dcf1f210
alexisrhc <alexisrhc@hotmail.com>
2019-10-14 17:21:31 -0300

    update

--- resources/views/layouts/appPrintedInvoice.blade.php
+++ resources/views/layouts/appPrintedInvoice.blade.php
@@ -17,7 +17,14 @@
     <nav class="navbar navbar-top navbar-expand-md navbar-dark" id="navbar-main">
       <div class="container-fluid">
         <!-- Brand -->
-        <a class="h4 mb-0 text-white text-uppercase d-none d-lg-inline-block" href="{{ route('home') }}">Inicio</a>
+        <div class="row">
+          <div class="col-5">
+            <a class="h4 mb-0 text-white text-uppercase d-none d-lg-inline-block" href="{{ route('home') }}">Inicio</a>
+          </div>
+          <div class="col-5">
+            <a class="h4 mb-0 text-white text-uppercase" href="{{ route('bill') }}">facturas</a>
+          </div>
+        </div>
         <ul class="navbar-nav align-items-center d-none d-md-flex">
           <li class="nav-item dropdown">
             <a class="nav-link pr-0" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
