a8505a893e75f2acc41d0f27d9f99573feebbc86
alexisrhc <alexisrhc@hotmail.com>
2019-10-14 19:08:10 -0300

    update 14/10

--- app/Http/Controllers/BillController.php
+++ app/Http/Controllers/BillController.php
@@ -65,6 +65,7 @@ class BillController extends Controller
         $bills->id_user = $request->id_user;
         $bills->id_client = $request->id_client;
         $bills->id_bill_temporal = $request->id_bill_temporal;
+        $bills->status = $request->status;
         $bills->save();
         return response()->json($bills);
     }
